ae90ad6fc2e92e012285599d1aedf123d2f8534a
elina-mns <ellie.mansurova@gmail.com>
2021-09-09 10:04:23 -0400

    add model and view model

---
+++ Memorize/Model/EmojiMemoryGameViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiMemoryGameViewModel.swift
+//  Memorize
+//
+//  Created by Elina Mansurova on 2021-09-08.
+//
+
+import SwiftUI
+
+class EmojiMemoryGameViewModel {
+    private(set) var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4)
+    
+}
--- Memorize/Model/MemoryGame.swift
+++ Memorize/Model/MemoryGame.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
-struct MemoryGame {
+struct MemoryGame<CardContent> {
+    private(set) var cards: Array<Card>
     
+    func choose(_ card: Card) {
+        
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        cards = Array<Card>()
+        // add number of pairs of cards * 2 cards to cards array
+        
+    }
+    
+    //by nesting Card struct we put it inside the Memory Game: MemoryGame.Card
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
 }
