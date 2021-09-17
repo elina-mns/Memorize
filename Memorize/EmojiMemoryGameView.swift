842ce135a504ec665099d198da3275ffc6365db9
elina-mns <ellie.mansurova@gmail.com>
2021-09-17 19:10:06 -0400

    add init, geometry reader

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -13,11 +13,15 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         AspectVGrid(items: gameViewModel.cards, aspectRatio: 2/3, content: { card in
-            CardView(card: card)
-                .padding(4)
-                .onTapGesture {
-                    gameViewModel.choose(card)
-                }
+            if !card.isFaceUp && card.isMatched {
+                Rectangle().opacity(0)
+            } else {
+                CardView(card: card)
+                    .padding(4)
+                    .onTapGesture {
+                        gameViewModel.choose(card)
+                    }
+            }
         })
         .foregroundColor(.red)
         .padding(.horizontal)
@@ -47,9 +51,9 @@ struct CardView: View {
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 20
+        static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.8
+        static let fontScale: CGFloat = 0.75
     }
 }
 
